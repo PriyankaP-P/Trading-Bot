ee92f1689ff209c653b6cc72706b81a2dde48bbf
Priyanka <p.rathor11@yahoo.com>
2020-07-24 13:16:02 +0000

    personalApi.js edited online with Bitbucket

--- personalApi.js
+++ personalApi.js
@@ -3,8 +3,8 @@
 const ccxt = require("ccxt");
 
 let binance = new ccxt.binance({
-  apiKey: "Ll8IQXn6q4ejxCM1QbQSUhUHqKR1ClRFh8U9YOACtw8hnwBGfZ9cpXTGmurVF1cl",
-  secret: "5nIYua2pdA2muFNt40JaksHRtqIXmzk38MGMwePPEeW2uKvB48BQNRjCKaaUU0k4"
+  apiKey: "****************",
+  secret: "****************"
 });
 
 module.exports = binance;
